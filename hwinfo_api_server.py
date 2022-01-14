71bc8d9b6c08b16d7cf18568fd96952f07c0efe2
Aleksandr Lobanov <anklav24@gmail.com>
2022-01-15 02:04:55 +0700

    feat(flask): add waitress.serve instead app.run

--- hwinfo_api_server.py
+++ hwinfo_api_server.py
@@ -1,16 +1,13 @@
-from app import app
-from app.third_party.process_control import kill_processes, run_processes
+from waitress import serve
+from api import app
+from api.third_party.process_control import kill_processes, run_processes
 
 from config import FLASK_HOST, FLASK_PORT, REMOTE_HWINFO_PORT
-
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.config["JSON_SORT_KEYS"] = False
-app.config["FLASK_ENV"] = 'development'
 
 if __name__ == '__main__':
     try:
         run_processes(REMOTE_HWINFO_PORT)
-        app.run(host=FLASK_HOST, port=FLASK_PORT)
+        serve(app, host=FLASK_HOST, port=FLASK_PORT)
         kill_processes()
     except Exception as e:
-        print(e)
+        app.logger.exception(e)
