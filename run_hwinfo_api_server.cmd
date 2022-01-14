8724bf4649e96ece9498bc999e5953c3a4c57f0c
Aleksandr Lobanov <anklav24@gmail.com>
2022-01-14 19:56:10 +0700

    refactor(flask): rename script

--- run_hwinfo_api_server.cmd
+++ run_hwinfo_api_server.cmd
@@ -1,1 +1,1 @@
-cd %~dp0 && python -m venv venv && ".\venv\Scripts\activate" && pip install -r requirements.txt && python hwinfo_api_server.py & deactivate < nul
+cd %~dp0 & python -m venv venv & ".\venv\Scripts\activate" && pip install -r requirements.txt && python hwinfo_api_server.py & deactivate < nul
