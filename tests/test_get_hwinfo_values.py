0e2e86d153954a2b631a20f3c978af48673ff095
Aleksandr Lobanov <anklav24@gmail.com>
2022-01-14 17:31:50 +0700

    test(flask): add tests for endpoints

---
+++ tests/test_get_hwinfo_values.py
@@ -0,0 +1,70 @@
+import pytest
+import requests
+
+from get_hwinfo_values import FLASK_HOST, FLASK_PORT
+
+if FLASK_HOST == '0.0.0.0':
+    FLASK_HOST = '127.0.0.1'
+
+
+# noinspection HttpUrlsUsage
+def get_request(method):
+    response = requests.get(f'http://{FLASK_HOST}:{FLASK_PORT}{method}')
+    return response
+
+
+def test_root():
+    response = get_request('/')
+    assert response.status_code == 200
+    assert len(response.json()['sensors']) > 0
+    assert len(response.json()['readings']) > 0
+
+
+def test_docs():
+    response = get_request('/docs')
+    assert response.status_code == 200
+
+    words = ['table', 'Method', 'Description']
+    for word in words:
+        assert word in response.text
+
+
+def test_status():
+    response = get_request('/status')
+    assert response.status_code == 200
+    assert response.json()['message'] == 'All systems are working'
+
+
+def test_hardware_lld():
+    response = get_request('/hardware_lld')
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+    keys = ['{#SENSORNAMEUSER}', "{#SENSORINDEX}", "{#LABELUSER}",
+            "{#READINGINDEX}", "{#READINGTYPENAME}", "{#READINGTYPE}",
+            "{#VALUE}", "{#UNIT}"]
+    for key in keys:
+        assert key in response.json()[0]
+
+
+def test_hardware_lld_unit():
+    response = get_request('/hardware_lld?unit=rpm')
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+    assert response.json()[2]["{#UNIT}"].lower() == 'rpm'
+
+
+def test_hardware_inventory():
+    response = get_request('/hardware_inventory')
+    assert response.status_code == 200
+    assert 'System' in response.text
+
+
+def test_value_lld():
+    response = get_request('/value_lld/255')
+    assert response.status_code == 200
+    assert len(response.text)
+
+
+if __name__ == '__main__':
+    pytest.main()
