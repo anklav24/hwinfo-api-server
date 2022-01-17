279c6fe68576f0f002ce41ac216e4e534ace7f4e
Aleksandr Lobanov <anklav24@gmail.com>
2022-01-17 23:59:19 +0700

    build: add 7-zip

--- build.ps1
+++ build.ps1
@@ -1,7 +1,5 @@
-taskkill /f /im remotehwinfo.exe
-taskkill /f /im HWiNFO32.exe
-
 rm -Recurse -Force "Zabbix Agent\user_scripts\hwinfo_api_server\"
+rm -Recurse -Force "Zabbix Agent\hwinfo_api_server.zip"
 rm -Recurse -Force .\build\
 
 pyinstaller.exe --onefile hwinfo_api_server.py --distpath "Zabbix Agent\user_scripts\hwinfo_api_server" --add-data "api/templates;api/templates" --add-data "api/static;api/static" --add-data "api/third_party;api/third_party"
@@ -13,3 +11,8 @@ cp .\README.md "Zabbix Agent\user_scripts\hwinfo_api_server\README.md"
 rm -Recurse -Force "Zabbix Agent\user_scripts\hwinfo_api_server\api\third_party\process_control.py"
 rm -Recurse -Force .\build\
 rm -Recurse -Force hwinfo_api_server.spec
+
+# Install 7-ZIP and add to environment variables 7z
+cd 'Zabbix Agent'
+7z a -tzip 'hwinfo_api_server.zip'
+cd ..
